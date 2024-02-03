a33a78c736ab32e246bd0ed8a4d12392ae44103e
Clara_b64 <main@ctngamestudio.ovh>
2024-02-02 16:39:18 -0800

    Change example

--- example/vrui_iter.c
+++ example/vrui_iter.c
@@ -15,8 +15,37 @@
 #include "../vrui.h"
 #include "stdio.h"
 
+static vrui_mutex* MutexPtr;
+
 void vrui_log_impl(const char* Log){
 	printf("%s\n", Log);
+
+}
+
+void vrui_func_impl(vrui_onrender_info Onrenderinfo) {
+	vrui_lock_mutex(Onrenderinfo.UserMutex);
+	vrui_window* WindowPtr = Onrenderinfo.CurrWindow;
+	
+	vrui_rect TestRect;
+	TestRect.X = 0.2;
+	TestRect.Y = 0.2;
+	TestRect.Z = 0.2;
+	TestRect.A = 0.8;
+	TestRect.B = 0.8;
+	TestRect.G = 0.8;
+
+	vrui_tex TestTexture;
+	vrui_uva TestUvc;
+	TestUvc.X = 0.2;
+	TestUvc.Y = 0.2;
+	TestUvc.Z = 0.8;
+	TestUvc.W = 0.8;
+
+	vrui_gfx_quad(WindowPtr, &TestRect, TestTexture, TestUvc, false);
+	vrui_gfx_quad(WindowPtr, &TestRect, TestTexture, TestUvc, false);
+	vrui_gfx_quad(WindowPtr, &TestRect, TestTexture, TestUvc, false);
+
+	vrui_unlock_mutex(Onrenderinfo.UserMutex);
 
 }
 
@@ -33,6 +62,11 @@ int main(int argc, char* argv[]){
 
 	vrui_window* TestWindow;
 	vrui_job TestJob;
+	TestJob.DataPtr = NULL;
+	TestJob.SteadyPtr = NULL;
+	MutexPtr = &TestJob.Mutex;
+	TestJob.FuncPtr = &vrui_func_impl;
+
 	TestWindow = vrui_new_window(TestJob, "test_window");
 
 	vrui_window_debug_dump_console(TestWindow);
@@ -52,11 +86,14 @@ int main(int argc, char* argv[]){
 	TestUvc.Z = 0.9;
 	TestUvc.W = 0.9;
 
+	vrui_update();
+
 	vrui_gfx_quad(TestWindow, &TestRect, TestTexture, TestUvc, false);
 	vrui_gfx_quad(TestWindow, &TestRect, TestTexture, TestUvc, false);
 
 	vrui_window_debug_dump_console(TestWindow);
 	vrui_gfx_grow_vert(TestWindow, 4680, 45000, 10000, 10000);
+	vrui_update();
 	vrui_window_debug_dump_console(TestWindow);
 
 	return 0;
