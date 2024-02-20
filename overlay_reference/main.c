1e3b619f280e2e6a6b94d476d1fa7de043f90a19
Clara_b64 <main@ctngamestudio.ovh>
2024-02-20 05:48:24 -0800

    Add official renderer simple implementation

---
+++ overlay_reference/main.c
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  SVRUI Source File.
+//  Copyright (C) 2024 Clara Lille
+// -------------------------------------------------------------------------
+//  File name:   main.c
+//  Version:     v1.00
+//  Created:     19/02/24 by Clara.
+//  Description: 
+// -------------------------------------------------------------------------
+//  This project is licensed under the MIT License
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include "renderer.h"
+
+int main(int argc, char* argv[]){
+	init_renderer();
+
+	return 0;
+
+}
